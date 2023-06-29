00d1a7e30f59b8a4ec9a15037806c33e3870afcb
Jerry Chen <jerry@apache.org>
2023-06-29 10:06:47 -0500

    searchPost supports count & offset

--- cloudflare-worker/bsky-search.js
+++ cloudflare-worker/bsky-search.js
@@ -1,11 +1,17 @@
 import { fetchGuarded } from "./bsky-fetch-guarded";
 
-export async function searchPost(searchTerm) {
+export async function searchPost(searchTerm, params) {
+  let urlParams = {
+    q: searchTerm,
+  };
+  if (params.count !== undefined) {
+    urlParams.count = params.count;
+  }
+  if (params.offset !== undefined) {
+    urlParams.offset = params.offset;
+  }
   let url =
-    "https://search.bsky.social/search/posts?" +
-    new URLSearchParams({
-      q: searchTerm,
-    });
+    "https://search.bsky.social/search/posts?" + new URLSearchParams(urlParams);
   let response = await fetchGuarded(url);
   if (response !== null) {
     return response.json();
